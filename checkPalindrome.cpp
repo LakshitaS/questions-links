53bbeb55ae6d7c08a231f07241ede9e9f47dda4f
Lakshita Saxena <97253226+LakshitaS@users.noreply.github.com>
2023-07-20 17:15:41 +0530

    isalnum | 'A'-'a'

---
+++ checkPalindrome.cpp
@@ -0,0 +1,40 @@
+//isalnum function can be used to check if a char is an alphabet or a number.
+//https://www.codingninjas.com/studio/problems/check-if-the-string-is-a-palindrome_1062633?leftPanelTab=0
+
+bool checkPalindrome(string s)
+{	
+    for (int i = 0; i < s.length(); i++)
+    {
+        if (s[i] <= 'Z' && s[i] >= 'A')
+        {
+            s[i] -= ('A' - 'a');
+            
+        }
+    }
+
+    int i = 0;
+    int j = s.length() - 1;
+
+    while (i < j)
+    {
+        if (isalnum(s[i]) == 0)
+        {
+            ++i;
+        }
+        else if (isalnum(s[j]) == 0)
+        {
+            // jth pointer points to invalid character.
+            --j;
+        }
+        else if (s[i] == s[j])
+        {
+            ++i, --j;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    return true;
+}
