1365a68a901e901e7fd673dec8b381ab1bc43372
Lakshita Saxena <97253226+LakshitaS@users.noreply.github.com>
2023-07-22 12:22:00 +0530

    Sieve Of Eratothons ✔

---
+++ 204. Count Primes.cpp
@@ -0,0 +1,28 @@
+// https://leetcode.com/problems/count-primes/description/
+
+// Given an integer n, return the number of prime numbers that are strictly less than n.
+
+class Solution {
+public:
+	int countPrimes(int n) {
+		//prime no. generation using sieve of eratothons
+		vector<bool> prime(n + 1, true);
+		prime[0] = false;
+		prime[1] = false;
+		for (int i = 2; i * i <= n; i++) {
+			if (prime[i]) {
+				for (int j = i * i; j <= n; j += i) {
+					prime[j] = false;
+				}
+			}
+		}
+		//counting prime numbers
+		int primeCount = 0;
+		for (int i = 2; i < n; i++) {
+			if (prime[i]) {
+				primeCount++;
+			}
+		}
+		return primeCount;
+	}
+};
